7654feeafa4a75fe464a13bcaace3553002a718d
Compuespi2016 <computacaouespi2016@gmail.com>
2018-09-13 16:52:14 -0300

    conexao

---
+++ db/conexao.php
@@ -0,0 +1,9 @@
+<?php
+	$usuario = 'compuespi2016';
+	$senha = 'computacao';
+	$host = 'db4free.net:3306';
+	$dbname = 'compuespi';
+
+	$conecta = mysqli_connect($host,$usuario,$senha,$dbname);
+
+?>
