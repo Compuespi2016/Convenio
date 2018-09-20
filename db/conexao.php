89f1372a8687b0183edbceda43ebf9f1c893b8d7
Compuespi2016 <computacaouespi2016@gmail.com>
2018-09-20 17:17:20 -0300

    ajustes css

--- db/conexao.php
+++ db/conexao.php
@@ -1,22 +1,8 @@
 <?php
-<<<<<<< HEAD
 	$usuario = 'root';
 	$senha = '';
 	$host = 'localhost';
 	$dbname = 'convenio';
-=======
-<<<<<<< HEAD
-	$usuario = 'root';
-	$senha = '';
-	$host = '761ac413.ngrok.io';
-	$dbname = 'convenio';
-=======
-	$usuario = 'compuespi2016';
-	$senha = 'computacao';
-	$host = 'db4free.net';
-	$dbname = 'compuespi';
->>>>>>> e49b03562bc39bb100c43d32cf3b07286879be87
->>>>>>> f0f50f82b1214f2fda74adf316f8b077cc5f1d0d
 
 	$conecta = mysqli_connect($host,$usuario,$senha,$dbname);
 	
