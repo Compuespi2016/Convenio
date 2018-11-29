9f31ea4eec2bbe8855bb2c7be17017875e9fa143
Compuespi2016 <computacaouespi2016@gmail.com>
2018-11-29 15:36:24 -0300

    finalizacao ultimo sprint

--- formularios/atualizar_plano_estagio.php
+++ formularios/atualizar_plano_estagio.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 include_once('../db/conexao.php');
 session_start();
 	if(isset($_POST['plano'])){
@@ -6,11 +6,22 @@ session_start();
 		$id_empresa = $_SESSION["id"];
 		$plano = $_POST["plano"];
 		$avaliacao = $_POST["avaliacao"];
-		$nota = $_POST["nota"];
 		
-		$updateplano_id = "UPDATE plano_estagio SET id_aluno=".$id_aluno.",id_empresa=".$id_empresa. ",plano= '$plano',avaliacao='$plano',nota=".$nota . "	WHERE aluno_id=".$id_aluno;
+		echo $updateplano_id = "UPDATE plano_estagio SET aluno_id=".$id_aluno.",empresa_id=".$id_empresa. ",plano= '$plano',avaliacao='$avaliacao' WHERE aluno_id=".$id_aluno;
 		$updateplano_id = mysqli_query($conecta,$updateplano_id);
 
+		if(!$updateplano_id){
+			echo mysqli_error($conecta);
+		}
+
+		$dados_aluno = $_POST["id_aluno"];
+		$dados_aluno = "SELECT * FROM aluno WHERE id=".$dados_aluno;
+		$dados_aluno = mysqli_query($conecta,$dados_aluno);
+		$dados_aluno = mysqli_fetch_assoc($dados_aluno);
+
+		$dados_vinculo = "SELECT * FROM plano_estagio WHERE aluno_id=".$_POST['id_aluno']." AND empresa_id=".$_SESSION['id'];
+		$dados_vinculo = mysqli_query($conecta,$dados_vinculo);
+		$dados_vinculo = mysqli_fetch_assoc($dados_vinculo);
 	}
 
 	if(isset($_GET['id_aluno'])){
@@ -34,7 +45,6 @@ session_start();
 	<link href="../estilos/topo.css" rel="stylesheet">
 	<script>
 		function validanota(elem){
-
 			if(parseInt(elem.value) > 10){
 				elem.value = 10
 			}
@@ -43,20 +53,24 @@ session_start();
 </head>
 <body>
 	<?php require('../include/topo.php') ?>
-	<form id="cadastro" action="criar_plano_estagio.php" method="POST">
-		<div id="titulo_divisao">Criar plano de estágio</div>
+	<div id="menu_left" style="display:flex;flex-direction:column;">
+		<a href="../frequencia.php" style="text-align:start;width:100px;color:white;text-decoration:none;padding:5px;background-color:#2268b2;position:relative;">Voltar ></a>
+	</div>
+	<form id="cadastro" action="atualizar_plano_estagio.php" method="POST">
+		<div id="titulo_divisao">Atualizar plano de estágio</div>
 		<p style="color:white;font-size: 20px;font-weight: 200">Aluno(a): <?php echo $dados_aluno["nome"]; ?></p>
-
-		<input type="hidden" name="id_aluno" maxlength=50 value="<?php echo $_GET['id_aluno'] ?>">
+		
+		<?php $id = isset($_GET['id_aluno']) ? $_GET['id_aluno'] : $_POST['id_aluno']; ?>
+		<input type="hidden" name="id_aluno" maxlength=50 value="<?php echo $id ?>">
 
 		<label for="plano" style="color:white">Plano de estágio</label>
 		<textarea type="text" cols="40" name="plano" maxlength=535><?php echo $dados_vinculo['plano']; ?></textarea>
 
 		<label for="avaliacao" style="color:white">Avaliação</label>
-		<textarea type="text" cols="40" name="avaliacao" maxlength=535 value="<?php echo $dados_vinculo['avaliacao']; ?>"></textarea>
+		<textarea type="text" cols="40" name="avaliacao" maxlength=535><?php echo $dados_vinculo['avaliacao']; ?></textarea>
 
 		<label for="nota" style="color:white">Nota</label>
-		<input type="number" name="nota" max=10 maxlength=3 value="<?php echo $dados_vinculo['nota']; ?>" onchange="validanota(this)">
+		<p><?php echo $dados_vinculo['nota']; ?></p>
 		
 		<div id="divisao"></div>
 
